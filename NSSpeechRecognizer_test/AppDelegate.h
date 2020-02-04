3a99fe4fa4da803e6edb651e0a379160a6b28dd5
Gregory Casamento <greg.casamento@gmail.com>
2020-02-04 14:54:27 -0500

    Initial Commit

---
+++ NSSpeechRecognizer_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSSpeechRecognizer_test
+//
+//  Created by Gregory Casamento on 2/4/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSSpeechRecognizer_test/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  NSSpeechRecognizer_test
+//
+//  Created by Gregory Casamento on 2/4/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
